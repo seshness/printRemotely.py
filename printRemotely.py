be968f2fd75b4b523a52323a01fa930e9994b10e
Seshadri Mahalingam <seshadri@berkeley.edu>
2011-04-26 14:01:52 -0700

    Added -d flag to delete the file on the server after printing. (Feature credit: Akihiro Matsukawa)

--- printRemotely.py
+++ printRemotely.py
@@ -17,9 +17,10 @@ SYNOPSIS
     printRemotely.py [OPTIONS] [user@]printserver file
 
 OPTIONS:
-    -h / --help:               Display this help
-    -i SSH_PRIVATE_KEY:        Use this private key for scp and ssh connections
-    -P PRINTERNAME:            Use the specified printer
+    -h / --help               Display this help
+    -i SSH_PRIVATE_KEY        Use this private key for scp and ssh connections
+    -P PRINTERNAME            Use the specified printer
+    -d                        Delete the file on the server after printing
 
 INSPIRATION:
     Karan Malik
@@ -32,10 +33,11 @@ AUTHOR:
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], 
-                               "hi:P:Z:",
+                               "hi:P:Z:d",
                                ["help", "duplex"])
     sshOptions = ""
     lprOptions = ""
+    deleteFile = False
     
     for o,a in opts:
         if o in ('-h', '--help'):
@@ -47,17 +49,22 @@ try:
         elif o == "--duplex":
             if lprOptions.find("-Z duplex") == -1:
                 lprOptions += " " + "-Z duplex"
+        elif o == "-d":
+            deleteFile = True
 
     if len(args) != 2:
         print "Invalid arguments. Try --help."
         sys.exit(2)
 
+    filename = args[1].split('/')[-1]
+
     scpCommand = "scp" + (" " + sshOptions if sshOptions != "" else "") + \
         ' "' + args[1] + '" ' + args[0] + ":"
-    lprCommand = "lpr" + (" " + lprOptions if lprOptions != "" else "") + \
-        ' "' + args[1].split('/')[-1] + '"'
+    lprCommand = "ls" + (" " + lprOptions if lprOptions != "" else "") + \
+        ' "' + filename + '"'
     sshCommand = 'ssh' + (" " + sshOptions if sshOptions != "" else "") + \
-        ' ' +  args[0] + ' "' + lprCommand + '"'
+        ' ' +  args[0] + ' "' + lprCommand + \
+        ("; rm " + filename if deleteFile else "")  + '"'
 
     print scpCommand
     subprocess.call(scpCommand, shell=True)
